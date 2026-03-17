095febbf65c246f3224dcd48bb5cdda6dacdd05d
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2026-03-17 16:42:24 +0000

    Add __init__ for rules

---
+++ src/devsec_lint/rules/__init__.py
@@ -0,0 +1,5 @@
+from src.devsec_lint.rules.github_actions import RULES as GITHUB_ACTIONS_RULES
+
+ALL_RULES = [
+    *GITHUB_ACTIONS_RULES,
+]
---
+++ src/devsec_lint/rules/github_actions/__init__.py
@@ -0,0 +1,5 @@
+from src.devsec_lint.rules.github_actions.unpinned_action import UnpinnedActionRule
+
+RULES = [
+    UnpinnedActionRule(),
+]
